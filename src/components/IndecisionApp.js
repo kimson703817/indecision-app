371e94f4d7fc9e946d31433ec98645bbf8a3581d
sunny <sunnyyorita@gmail.com>
2018-08-23 18:48:34 -0700

    styling the app with scss

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -22,18 +22,21 @@ export default class IndecisionApp extends React.Component {
                 <Header
                     subtitle={subtitle}
                 />
-                <Action
-                    hasOptions={this.state.options.length}
-                    handlePick={this.handlePick}
-                />
-                <Options
-                    options={this.state.options}
-                    handleDeleteOptions={this.handleDeleteOptions}
-                    handleDeleteOption= {this.handleDeleteOption}
-                />
-                <AddOption
-                    handleAddOption={this.handleAddOption}
-                />
+                <div className="container">
+                    <Action
+                        hasOptions={this.state.options.length}
+                        handlePick={this.handlePick}
+                    />
+                    <Options
+                        options={this.state.options}
+                        handleDeleteOptions={this.handleDeleteOptions}
+                        handleDeleteOption= {this.handleDeleteOption}
+                    />
+                    <AddOption
+                        handleAddOption={this.handleAddOption}
+                    />
+                </div>
+                
                 <OptionModal
                     selectedOption={this.state.selectedOption}
                     handleDeselectOption={this.handleDeselectOption}
